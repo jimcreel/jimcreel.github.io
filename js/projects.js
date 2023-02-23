2c7f8c4f79621a0e339d312afe379662ce54c68b
jimcreel <jim.creel@gmail.com>
2023-02-23 12:18:41 -0800

    updated modal on projects page

--- js/projects.js
+++ js/projects.js
@@ -7,3 +7,18 @@ spaceBattle.addEventListener('mouseover', () => {
 spaceBattle.addEventListener('mouseout', () => {
     spaceBattle.src = "img/spaceBattleStatic.png"
 });
+
+const openBtn = document.getElementById("disneySite");
+const modal = document.getElementById("modal");
+const close = document.getElementById("close");
+
+const openModal = () => {
+	modal.style.display = "block";
+};
+
+const closeModal = () => {
+	modal.style.display = "none";
+};
+
+openBtn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
